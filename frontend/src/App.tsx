4ecda991f43332b88fdc9febb3b36bfc1c99fde9
Renato Reino <rreino@outlook.com>
2021-09-10 23:04:55 -0300

    Alteração do Titulo da lista para Todas as Vendas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </div>
 
         <div className="py-3">
-          <h2 className="text-primary">Todas Vendas</h2>
+          <h2 className="text-primary">Todas as Vendas</h2>
         </div>
         
         <DataTable />
